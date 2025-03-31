f82b03fe12345e84da930ea0c6db9b6305b92554
Eckphil <tsky3674@gmail.com>
2025-03-31 11:52:25 +0900

    Splash Added

---
+++ app/src/main/java/com/example/practice/ui/screens/splash/Splash.kt
@@ -0,0 +1,39 @@
+package com.example.practice.ui.screens.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.practice.R
+import com.example.practice.ui.theme.White
+
+@Preview
+@Composable
+fun Splash(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(White),
+        contentAlignment = Alignment.Center,
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.melog),
+            contentDescription = "Test Image",
+            modifier = Modifier
+                .width(283.16547.dp)
+                .height(160.dp)
+        )
+    }
+}
